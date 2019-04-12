7f8f74d05a48d50d5d10612ed0dcc46faae17e67
[mediba] Satoshi Takeda <takeda@mediba.jp>
2019-04-12 12:40:03 +0900

    [from now] 2019/04/12 12:40:03
     2019-04-17-frontend-learn-and-delegation/deck.mdx | 34 +++++++++++++++++++----
     theme.js                                          | 12 ++++++--
     2 files changed, 38 insertions(+), 8 deletions(-)

--- theme.js
+++ theme.js
@@ -4,10 +4,16 @@ import okaidia from 'react-syntax-highlighter/styles/prism/okaidia'
 export default {
     ...yellow,
     colors: {
-        text: '#fff',
-        background: '#E1690D'
+        text: '#333',
+        background: '#eee'
     },
     prism: {
         style: okaidia
+    },
+    h1: {
+        textTransform: 'none'
+    },
+    h2: {
+        textTransform: 'none'
     }
   }
