8ca9fb72c1fa1125a6eea93bb03ecee9da9e6eca
Satoshi Takeda <tkdnation@gmail.com>
2019-04-16 06:11:36 +0900

    add: bvinside-vol8 session slide

--- theme.js
+++ theme.js
@@ -4,16 +4,33 @@ import okaidia from 'react-syntax-highlighter/styles/prism/okaidia'
 export default {
     ...yellow,
     colors: {
-        text: '#333',
-        background: '#eee'
+        text: '#222',
+        background: '#E2E1DC'
     },
     prism: {
         style: okaidia
     },
-    h1: {
+    Slides: {
+        padding: "0"
+    },
+    heading: {
         textTransform: 'none'
     },
+    h1: {
+        fontSize: '2.5em'
+    },
     h2: {
-        textTransform: 'none'
+        fontSize: '2em'
+    },
+    img: {
+        maxWidth: '80%'
+    },
+    blockquote: {
+        fontSize: "0.7em",
+        fontWeight: "normal",
+        fontStyle: "italic",
+        backgroundColor: "#ccc",
+        borderLeft: "8px solid #aaa",
+        lineHeight: 2
     }
-  }
+}
